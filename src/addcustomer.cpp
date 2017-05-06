293e5fcb9d6fe955c5b0c7242c7340cc2bbe3e82
Pardis Khodadoustan <Pardis Khodadoustan>
2017-05-06 15:33:15 -0700

    improving addCustomer

--- src/addcustomer.cpp
+++ src/addcustomer.cpp
@@ -31,32 +31,35 @@ void AddCustomer::on_pushButton_clicked()
      */
     //DBCustomers variable should be reference since getCustomer returns by reference
     std::vector<Customer>& DBCustomers = DbManager::getInstance()->getCustomers();
-   if(ui->lineEdit_key->text()!="0" && ui->lineEdit_key->text()!="1"){
-        ui->label_message->setText("Not a valid value for key, please enter 0, or 1");
-        return;
-    }
-   if(ui->lineEdit_Interest->text()!="0" && ui->lineEdit_Interest->text()!="1"&& ui->lineEdit_Interest->text()!="2" && ui->lineEdit_Interest->text()!=""){
-        ui->label_message->setText("Not a valid value for Interest, please enter 0: not interested,  1: somewhat interested, 2: very interested");
-        return;
-    }
     Customer customer;
-    customer.setName( ui->lineEdit_companyName->text());
-    customer.setCustomerAddress(ui->lineEdit_streetAddress->text(),ui->lineEdit_city->text(), ui->lineEdit_state->text(),ui->lineEdit_zip->text());
-    customer.setValue(ui->lineEdit_key->text().toInt());//add: check if not 0 or 1 ...
-    customer.setInterest((Interest)ui->lineEdit_Interest->text().toInt());
+    /*check if customer is not repeated*/
     for(unsigned int i = 0; i<DBCustomers.size();i++)
     {
-        if(DBCustomers[i].getName().toLower()==customer.getName().toLower()){
+        if(DBCustomers[i].getName().toLower()==ui->lineEdit_companyName->text().toLower()){
             ui->label_message->setText("Not able to add the customer: repeated customer");
             return;
         }
     }
+    customer.setName( ui->lineEdit_companyName->text());
+    /*check if required field is empty*/
     if(customer.getName()=="" ||ui->lineEdit_streetAddress->text()==""||ui->lineEdit_city->text()==""||ui->lineEdit_state->text()==""||ui->lineEdit_zip->text()==""||ui->lineEdit_key->text()=="" ){
         ui->label_message->setText("Not able to add the customer: required field(s) is empty");
         return;
     }
+    /*check id value for key is invalid*/
+    if(ui->lineEdit_key->text()!="0" && ui->lineEdit_key->text()!="1"){
+        ui->label_message->setText("Not a valid value for key, please enter 0, or 1");
+        return;
+    }
+    customer.setCustomerAddress(ui->lineEdit_streetAddress->text(),ui->lineEdit_city->text(), ui->lineEdit_state->text(),ui->lineEdit_zip->text());
+    customer.setValue(ui->lineEdit_key->text().toInt());
+    /*check if value for interest is invalid*/
+     if(ui->lineEdit_Interest->text()!="0" && ui->lineEdit_Interest->text()!="1"&& ui->lineEdit_Interest->text()!="2" && ui->lineEdit_Interest->text()!=""){
+        ui->label_message->setText("Not a valid value for Interest, please enter 0: not interested,  1: somewhat interested, 2: very interested");
+        return;
+    }
+    customer.setInterest((Interest)ui->lineEdit_Interest->text().toInt());
     DBCustomers.push_back(customer);
-
     ui->lineEdit_companyName->clear();
     ui->lineEdit_streetAddress->clear();
     ui->lineEdit_city->clear();
@@ -65,6 +68,6 @@ void AddCustomer::on_pushButton_clicked()
     ui->lineEdit_Interest->clear();
     ui->lineEdit_key->clear();
     ui->label_message->setText("Customer Added Successfully");
-
+    /*updating sortBox (keeping current index)*/
     parentWindow->showCustomers((bool)parentWindow->sortBoxIndex());
 }
