a7a4d4b1fc014c5a3bec1a0ffca671f25fab99d9
Brian Clinkenbeard <bclinkenbeard0@saddleback.edu>
2017-05-15 14:58:00 -0700

    doxygen stuff

--- src/customer.h
+++ src/customer.h
@@ -6,11 +6,24 @@
 
 enum Interest {LOW=0, MEDIUM, HIGH};  // Customer's level of interest in our product
 
+/**
+ * @brief Customer data class
+ * Contains customer name, address, and interest information.
+ */
 class Customer
 {
 public:
+    /** Default constructor */
     Customer();
-    Customer(QString, Interest, bool, QString, QString, QString, QString);
+    /** non-default constructor */
+    Customer(QString name, /**< customer name */
+             Interest initInterest, /**< Interest in products */
+             bool key, /**< Key customer boolean */
+             QString,
+             QString,
+             QString,
+             QString);
+    /** copy constructor */
     Customer(const Customer&);
     void setName(QString);
     void setCustomerAddress(QString, QString, QString, QString);
@@ -21,6 +34,7 @@ public:
     Address getCustomerAddress() const;
     Interest getInterest() const;
     int getValue() const;
+    /** compares QStrings for sorting */
     bool operator <(const Customer&) const;
     bool operator >(const Customer&) const;
     void addProduct(Product item);
